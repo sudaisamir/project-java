74a52f7e72b3b47c6661c32e8024f4122c9e3d44
sudaisamir <139509085+sudaisamir@users.noreply.github.com>
2023-09-01 00:52:08 +0500

    JS

---
+++ java.js
@@ -0,0 +1,51 @@
+// !!!!!!!!!chaper # 1 !!!!!!!!!
+// ?answer 1
+alert('hello Welcome')
+
+// ?answer 2
+alert('Error!please enter a valid password.')
+
+// ?answer 3
+alert('Welcome to JS Land..\nHappy coding!')
+
+// ?answer 4
+alert('Welcome to JS Land...')
+alert('Happy coding!\n▢prevent this page from creating additional dialog')
+
+// ?answer 5
+let text;
+let text2 = prompt("Please enter your message:");
+ alert(text2) 
+
+
+
+
+// !!!!!!!!!chaper # 2 !!!!!!!!!
+
+// ?answer 1 and 2 
+var name='sudais'
+var name2='aamir'
+console.log( name + name2)
+
+// ?answer 3 
+alert('hello world')
+
+// ? answer 4
+alert('sudaisaamir')
+alert('age :25')
+alert('certified mobile application development')
+
+// ? answer 5
+alert('pizza \npizz \npiz \npi \np')
+    
+// ? answer 6
+var email='abc@gmail.com'
+alert('my email address is '+email)
+
+// ? answer7
+var book='I Am Trying To Learn From Smarter Way To Learn JS'
+alert(book)
+
+// ? answer 8  
+document.writeln('my name is sudais')
+    
